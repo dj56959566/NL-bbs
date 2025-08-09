c346e0df6bca1f17aa2993582e0b0dbf53fc5edf
dj56959566 <167070575+dj56959566@users.noreply.github.com>
2025-08-09 20:40:54 +0800

    Update nodeloc.js

--- nodeloc.js
+++ nodeloc.js
@@ -33,11 +33,11 @@ async function checkin(account, retryCount = MAX_RETRY) {
   const { COOKIE, CSRF, TG_TOKEN, TG_USER_ID, TG_PROXY, ALIAS } = account;
   console.log(`\n🧑 账号 [${ALIAS}] 开始签到，剩余重试次数：${retryCount}`);
   try {
-    const res = await axios.post("https://nodeloc.com/checkin", {}, {
+    const res = await axios.post("https://www.nodeloc.com/checkin", {}, {
       headers: {
         cookie: COOKIE,
-        origin: "https://nodeloc.com",
-        referer: "https://nodeloc.com/latest",
+        origin: "https://www.nodeloc.com",
+        referer: "https://www.nodeloc.com/latest",
         "user-agent": "Mozilla/5.0",
         "x-csrf-token": CSRF,
         "x-requested-with": "XMLHttpRequest",
